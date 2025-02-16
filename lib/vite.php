11751dcf096322d543f3a818c962eddd273d7036
Andrew Hale <services.hale.adh@gmail.com>
2025-02-16 18:05:28 -0500

    add check for vite running.  remove scss dep

--- lib/vite.php
+++ lib/vite.php
@@ -6,6 +6,7 @@ class WPVite {
 	public string $wpEnqueueId;
 	public string $server;
 	public string $entryPoint;
+	public bool $viteIsRunning;
 	public array $jsDeps = [];
 	public function __construct($isChild) {
 		$dir = $isChild ? get_stylesheet_directory() : get_template_directory();
@@ -16,11 +17,12 @@ class WPVite {
 		$this->wpEnqueueId = $env['WP_ENQUEUE_ID'];
 		$this->server = $env['VITE_PROTOCOL'] . '://' . $env['VITE_HOST'] . ':' . $env['VITE_PORT'];
 		$this->entryPoint = $env['VITE_ENTRY_POINT'];
+		$this->viteIsRunning = $env['WP_ENVIRONMENT'] == 'development' ? true : $this->checkServer();
 		$this->init();
 	}
 
 	public function init() {
-		$viteIsRunning = $this->checkServer();
+		$viteIsRunning = $this->viteIsRunning;
 		add_action('wp_enqueue_scripts', function () use ($viteIsRunning) {
 			if ($viteIsRunning === true) {
 				$this->viteDevAssets();
@@ -129,9 +131,7 @@ class WPVite {
 		$i = 0;
 		foreach ($filelist['js'] as $file) {
 			$i++;
-			$id = $this->wpEnqueueId . '-script-' . $i;
-			$path = $this->distUri . '/' . $file;
-			wp_enqueue_script($id, $path, $this->jsDeps, '1.0.0', ['strategy'  => 'defer']);
+			wp_enqueue_script($this->wpEnqueueId . '-script-' . $i, $this->distUri . '/' . $file, $this->jsDeps, '', true);
 		}
 	}
 }
